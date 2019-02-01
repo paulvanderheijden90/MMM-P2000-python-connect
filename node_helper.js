1a80ec1ba53a1ca2ef24001681fb6366e5a481ac
paulvanderheijden90 <40613554+paulvanderheijden90@users.noreply.github.com>
2019-02-01 13:36:33 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,1 +1,76 @@
-/// node_helper.jsconst spawn = require("child_process").spawnvar NodeHelper = require("node_helper")module.exports = NodeHelper.create({  socketNotificationReceived: function(notification, payload) {    switch(notification) {      case "GIVE_ME_DATA":        this.job()        break    }  },  job: function() {    var process = spawn("python", ["/home/pi/src/P2000.py"])    process.stdout.on("data", (data)=>{      console.log(data)      var result = String.fromCharCode.apply(null, new Uint16Array((data)))      this.sendSocketNotification("HERE_IS_DATA", result)    })  }})
+/// node_helper.js
+
+
+
+const spawn = require("child_process").spawn
+
+
+
+var NodeHelper = require("node_helper")
+var process = spawn("python", ["/home/pi/src/P2000.py"])
+
+
+
+
+
+
+module.exports = NodeHelper.create({
+
+
+
+  socketNotificationReceived: function(notification, payload) {
+
+
+
+    switch(notification) {
+
+
+
+      case "GIVE_ME_DATA":
+
+
+
+        this.job()
+
+
+
+        break
+
+
+
+    }
+
+
+
+  },
+
+
+
+  job: function() {
+
+
+
+    
+
+
+
+    process.stdout.on("data", (data)=>{
+
+      console.log(data)
+
+      var result = String.fromCharCode.apply(null, new Uint16Array((data)))
+
+      this.sendSocketNotification("HERE_IS_DATA", result)
+
+
+
+    })
+
+
+
+  }
+
+
+
+})
+
