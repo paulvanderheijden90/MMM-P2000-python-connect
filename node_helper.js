77ae92fbb188783f91a425f60294733dc0a3323c
sam detweiler <sdetweil@gmail.com>
2019-01-31 21:01:12 -0600

    update, working example

--- node_helper.js
+++ node_helper.js
@@ -1,1 +1,73 @@
-/// node_helper.jsconst spawn = require("child_process").spawnvar NodeHelper = require("node_helper")module.exports = NodeHelper.create({  socketNotificationReceived: function(notification, payload) {    switch(notification) {      case "GIVE_ME_DATA":        this.job()        break    }  },  job: function() {    var process = spawn("python", ["/home/pi/src/P2000.py"])    process.stdout.on("data", (data)=>{      console.log(data)      var result = String.fromCharCode.apply(null, new Uint16Array((data)))      this.sendSocketNotification("HERE_IS_DATA", result)    })  }})
+/// node_helper.js
+
+
+
+const spawn = require("child_process").spawn
+
+
+
+var NodeHelper = require("node_helper")
+var path = require ('path')
+
+
+module.exports = NodeHelper.create({
+
+
+
+  socketNotificationReceived: function(notification, payload) {
+
+
+
+    switch(notification) {
+
+
+
+      case "GIVE_ME_DATA":
+
+
+
+        this.job()
+
+
+
+        break
+
+
+
+    }
+
+
+
+  },
+
+
+
+  job: function() {
+
+
+
+    var process = spawn("/usr/bin/python", [path.join(__dirname, "testit.py")])
+
+		 process.stderr.on("data", (data)=>{
+			console.log("p2000 process errorr="+data)
+		})
+    process.stdout.on("data", (data)=>{
+
+      console.log("p2000 data ="+data)
+			
+      var result = String.fromCharCode.apply(null, new Uint16Array((data)))
+
+      this.sendSocketNotification("HERE_IS_DATA", result)
+
+
+
+    })
+
+
+
+  }
+
+
+
+})
+
